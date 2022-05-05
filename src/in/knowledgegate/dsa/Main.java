d25260b4266c720e028f23d64fcfaa07f2f919f6
Prashant Jain <prasja@microsoft.com>
2022-05-05 13:24:18 +0530

    First commit

---
+++ src/in/knowledgegate/dsa/Main.java
@@ -0,0 +1,7 @@
+package in.knowledgegate.dsa;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Friends, this is where we Kill DSA !!!!");
+    }
+}
