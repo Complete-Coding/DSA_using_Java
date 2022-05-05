13e8f86d67b39180c170fea2f52eb53fb3f1fed9
Prashant Jain <prasja@microsoft.com>
2022-05-05 13:42:44 +0530

    Time and Space Complexity

---
+++ src/in/knowledgegate/dsa/complexity/Sum.java
@@ -0,0 +1,12 @@
+package in.knowledgegate.dsa.complexity;
+
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println("Sum:" + sum(5, 6));
+        System.out.println("Sum:" + sum(8, 10));
+    }
+
+    private static int sum (int a, int b) {
+        return a + b;
+    }
+}
