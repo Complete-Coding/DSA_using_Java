293c32235fc4e33d5e3050e45176317f52aa8238
Prashant Jain <prasja@microsoft.com>
2022-05-08 12:11:12 +0530

    Added Greedy, Graph, SlidingWindow, BFS, Backtracking, DynamicProgramming

---
+++ src/in/knowledgegate/dsa/backtracking/NQueen.java
@@ -0,0 +1,102 @@
+package in.knowledgegate.dsa.backtracking;
+
+import java.util.Scanner;
+
+/**
+ * The n-queens puzzle is the problem of placing
+ * n queens on an n x n chessboard such that no
+ * two queens attack each other.
+ *
+ * Given an integer n, return any one solution
+ * to the n-queens puzzle. You may
+ * return the answer in any order.
+ *
+ * Each solution contains a distinct board
+ * configuration of the n-queens' placement,
+ * where 'Q' and '.' both indicate a queen and
+ * an empty space, respectively.
+ *
+ * Example 1:
+ * Input: n = 4
+ * Output: [["..Q.","Q...","...Q",".Q.."]]
+ *
+ * Example 2:
+ * Input: n = 1
+ * Output: [["Q"]]
+ *
+ * Constraints:
+ * 1 <= n <= 30
+ */
+public class NQueen {
+  private void printBoard(int[][] board) {
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        char out = board[i][j] == 1 ? 'Q' : '.';
+        System.out.print(" " + out + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  private boolean isMoveSafe(int[][] board,
+          int row, int col) {
+
+    for (int j = 0; j < col; j++) {
+      if (board[row][j] == 1) {
+        return false;
+      }
+    }
+
+    for (int i = row, j = col; i >= 0 && j >=0; i--, j--) {
+      if (board[i][j] == 1) {
+        return false;
+      }
+    }
+
+    for (int i = row, j = col; j >= 0 && i < board.length; j--, i++) {
+      if (board[i][j] == 1) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * This method is only responsible to put queen
+   * in a specific column.
+   */
+  private boolean nQueen(int[][] board, int col) {
+    if (col >= board.length) {
+      return true;
+    }
+
+    for (int i = 0;i < board.length; i++) {
+      if (isMoveSafe(board, i, col)) {
+        board[i][col] = 1;
+        if (nQueen(board, col + 1)) return true;
+        board[i][col] = 0;
+      }
+    }
+
+    return false;
+  }
+
+  private void nQueen(int size) {
+    int[][] board = new int[size][size];
+    if (!nQueen(board, 0)) {
+      System.out.println("Solution does not " +
+          "exist");
+    } else {
+      printBoard(board);
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    NQueen nQueen = new NQueen();
+    System.out.print("Enter size of board: ");
+    int size = scanner.nextInt();
+    nQueen.nQueen(size);
+  }
+}
---
+++ src/in/knowledgegate/dsa/dynamicprogramming/RodCutting.java
@@ -0,0 +1,61 @@
+package in.knowledgegate.dsa.dynamicprogramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given a rod of length n inches and an array
+ * of prices that includes prices of all pieces of
+ * size smaller than n. Determine the maximum
+ * value obtainable by cutting up the rod and
+ * selling the pieces. For example, if the length
+ * of the rod is 8 and the values of different pieces
+ * are given as the following, then the maximum
+ * obtainable value is 22 (by cutting in two
+ * pieces of lengths 2 and 6)
+ *
+ * Example:
+ * length   | 1   2   3   4   5   6   7   8
+ * --------------------------------------------
+ * price    | 1   5   8   9  10  17  17  20
+ * And if the prices are as following, then the
+ * maximum obtainable value is 24 (by cutting in
+ * eight pieces of length 1)
+ *
+ */
+public class RodCutting {
+  static int count = 0;
+
+  public static void main(String[] args) {
+    RodCutting rodCutting = new RodCutting();
+    int price[]
+        = new int[] { 1, 5, 8, 9, 10, 17, 17,
+        20, 24 };
+    int n = price.length;
+    Map<Integer, Integer> maxProfit =
+        new HashMap<>();
+    System.out.println(
+        "Maximum obtained value is "
+            + rodCutting.cutRod(price, n, maxProfit));
+    System.out.println("count is:" + count);
+  }
+
+  public int cutRod(int prices[], int length,
+      Map<Integer, Integer> maxProfit) {
+    if (length <= 0) return 0;
+    if (maxProfit.containsKey(length)) {
+      return maxProfit.get(length);
+    }
+    count++;
+    int maxVal = Integer.MIN_VALUE;
+
+    for (int i = 0; i < length; i++) {
+      maxVal = Math.max(maxVal,
+          prices[i] + cutRod(prices,
+              length - i - 1, maxProfit));
+    }
+
+    maxProfit.put(length, maxVal);
+    return maxVal;
+  }
+}
