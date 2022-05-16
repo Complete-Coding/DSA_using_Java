947c2fce7046f7b954024e7d48f2db2925e44e01
Prashant Jain <prasja@microsoft.com>
2022-05-16 20:09:01 +0530

    Added more String problems

---
+++ src/in/knowledgegate/dsa/strings/GenerateParentheses.java
@@ -0,0 +1,69 @@
+package in.knowledgegate.dsa.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given n pairs of parentheses, write a function
+ * to generate all combinations of well-formed
+ * parentheses.
+ *
+ * Example 1:
+ * Input: n = 3
+ * Output: ["((()))","(()())","(())()","()(())","()()()"]
+ *
+ * Example 2:
+ * Input: n = 1
+ * Output: ["()"]
+ *
+ * Constraints:
+ * 1 <= n <= 8
+ */
+public class GenerateParentheses {
+
+  public static void main(String[] args) {
+    GenerateParentheses obj =
+        new GenerateParentheses();
+    System.out.println("Here is the result:");
+    for (String combi :
+        obj.generateParenthesis(3)) {
+      System.out.print(combi + ", ");
+    }
+  }
+
+  public List<String> generateParenthesis(int n) {
+    List<String> combinations = new ArrayList<>();
+    generateAllCombinations(new char[n*2], 0,
+        combinations);
+    return combinations;
+  }
+
+  private void generateAllCombinations(char[] current,
+      int pos, List<String> combinations) {
+    if (pos == current.length) {
+      if (isValid(current)) {
+        combinations.add(new String(current));
+      }
+    } else {
+      current[pos] = '(';
+      generateAllCombinations(current, pos + 1,
+          combinations);
+      current[pos] = ')';
+      generateAllCombinations(current, pos + 1,
+          combinations);
+    }
+  }
+
+  private boolean isValid(char[] current) {
+    int counter = 0;
+    for (int i = 0; i < current.length; i++) {
+      if (current[i] == '(') {
+        counter++;
+      } else {
+        counter--;
+      }
+      if (counter < 0) return false;
+    }
+    return counter == 0;
+  }
+}
---
+++ src/in/knowledgegate/dsa/strings/ReverseString.java
@@ -0,0 +1,30 @@
+package in.knowledgegate.dsa.strings;
+
+/**
+ * Write a function that reverses a string. The
+ * input string is given as an array of characters
+ * s.
+ * You must do this by modifying the input array
+ * in-place with O(1) extra memory.
+ *
+ * Example 1:
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ *
+ * Example 2:
+ * Input: s = ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ * Constraints:
+ * 1 <= s.length <= 105
+ * s[i] is a printable ascii character.
+ */
+public class ReverseString {
+  public void reverseString(char[] s) {
+    for (int i = 0; i < s.length / 2; i++) {
+      char temp = s[i];
+      s[i] = s[s.length - 1 - i];
+      s[s.length - 1 - i] = temp;
+    }
+  }
+}
