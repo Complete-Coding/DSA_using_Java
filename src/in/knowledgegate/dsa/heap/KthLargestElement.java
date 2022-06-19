27f96eddbceecbd4e7efbf49729e46add352a661
Prashant Jain <prasja@microsoft.com>
2022-06-19 12:59:52 +0530

    Added sorting and heap problems

---
+++ src/in/knowledgegate/dsa/heap/KthLargestElement.java
@@ -0,0 +1,45 @@
+package in.knowledgegate.dsa.heap;
+
+import java.util.PriorityQueue;
+
+/**
+ * Given an integer array nums and an integer k,
+ * return the kth largest element in the array.
+ *
+ * Note that it is the kth largest element in the
+ * sorted order, not the kth distinct element.
+ *
+ * Example 1:
+ * Input: nums = [3,2,1,5,6,4], k = 2
+ * Output: 5
+ *
+ * Example 2:
+ * Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+ * Output: 4
+ *
+ * Constraints:
+ * 1 <= k <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ */
+public class KthLargestElement {
+  public static void main(String[] args) {
+    KthLargestElement kthLargest =
+        new KthLargestElement();
+    int[] nums = new int[]{3,2,3,1,2,4,5,5,6};
+    System.out.println("Kth largest is:" +
+        kthLargest.findKthLargest(nums, 6));
+  }
+
+  public int findKthLargest(int[] nums, int k) {
+    PriorityQueue<Integer> kLargest =
+        new PriorityQueue<>();
+    for (int num : nums) {
+      kLargest.add(num);
+      if (kLargest.size() > k) {
+        kLargest.poll();
+      }
+    }
+
+    return kLargest.peek();
+  }
+}
